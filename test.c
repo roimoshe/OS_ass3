8560cc236b8bfb4cf6177507dbdd8af4cb14b859
roi moshe <roimoshe@github.com>
2020-06-09 17:18:19 +0300

    remove shiri's test, write test1

--- test.c
+++ test.c
@@ -4,62 +4,67 @@
 #include "user.h"
 
 
-// our tests not  the real usertests Todo: delete!
-#define PGSIZE          4096    // bytes mapped by a page
 
-volatile int func(volatile int *tmp){
-  *tmp = *tmp + 1;
-  return 3;
-}
+#define PGSIZE 4096
 
 void
 test2(){
-   printf(1,"-------------Test 2-----------\n");
-   int number = 1;
+   printf(1,"<<< Test 2 >>>\n");
+   int number_ptr = 1;
    int pid = fork();
    if(pid < 0 ){
       printf(1, "error in fork!\n");
       exit();
    } else if(pid == 0){ //child
-     number = 2;
-     printf(1, "in child(suppose to be 2): number is %d\n", number);
+     number_ptr = 2;
+     printf(1, "in child(suppose to be 2): number_ptr is %d\n", number_ptr);
      exit();
    } else{
-     number = 3;
+     number_ptr = 3;
      sleep(10);
-     printf(1, "in parent(suppose to be 3): number is %d\n", number);
+     printf(1, "in parent(suppose to be 3): number_ptr is %d\n", number_ptr);
      wait();
    }
-   printf(1, "<<<< PASS Test 2 >>>>\n");
+   printf(1,"<<< PASS Test 2 >>>\n");
+}
+
+void
+test1(){
+  int *number_ptr = 0;
+  printf(1,"<<< Test 1 >>>\n");
+  printf(1,"call sbrk(4*PGSIZE)\n");
+  char *addr = sbrk(4*PGSIZE);
+  printf(1,"after call sbrk(4*PGSIZE)\n");
+  sleep(10);
+  printf(1,"set number_ptr to sbrk ret value plus 3*PGSIZE\n");
+  number_ptr = (int *)(addr + 3*PGSIZE);
+  printf(1,"access *number_ptr\n");
+  *number_ptr = 12;
+  printf(1, "*number_ptr = %d\n", *number_ptr);
+  printf(1,"call sbrk(PGSIZE)\n");
+  sbrk(PGSIZE);
+  printf(1,"after call sbrk(PGSIZE)\n");
+  printf(1,"access *number_ptr and increment it by 20\n");
+  *number_ptr = (*number_ptr) + 20;
+  printf(1, "*number_ptr = %d\n", *number_ptr);
+  printf(1,"call sbrk(12*PGSIZE)\n");
+  sbrk(12*PGSIZE);
+  printf(1,"after call sbrk(12*PGSIZE)\n");
+  number_ptr = (int *)(addr + PGSIZE * 10);
+  printf(1,"set number_ptr to first sbrk ret value plus 10*PGSIZE\n");
+  *number_ptr = 800;
+  printf(1,"access *number_ptr set it to 800\n");
+  (*number_ptr)++;
+  printf(1,"access *number_ptr and increment it by 1\n");
+  printf(1, "*number_ptr = %d\n", *number_ptr);
+  printf(1,"<<< PASS Test 1 >>>\n");
 }
 
 int
 main(int argc, char *argv[])
 {
   if(*argv[1]=='0' || *argv[1]=='1'){
-    printf(1,"-------------Task 1 Test-----------\n");
-    // sleep(100);
-    char *a = (char *)sbrk(4*PGSIZE);
-    printf(1,"-------------after sbrk1-----------\n");
-    // sleep(100);
-    volatile int *pointer;
-    pointer = (int *)(a + 3*PGSIZE);
-    printf(1,"---pointer = 0x%x, &pointer = 0x%x, a = 0x%x, &a = 0x%x, main = 0x%x---\n", pointer, &pointer, a, &a, main);
-    printf(1, "----accessing memory----\n");// problem ------------>
-    *pointer = 12;
-    printf(1, "----%d----\n", *pointer);
-    printf(1, "----allocating more memory----\n");
-    sbrk(PGSIZE);
-    printf(1, "----accessing memory----\n");
-    *pointer = (*pointer) + 8;
-    printf(1, "----%d----\n", *pointer);
-    sbrk(14*PGSIZE);
-    printf(1,"-------------after sbrk2-----------\n");
-    pointer = (int *)(a + PGSIZE * 10);
-    *pointer = 99;
-    func(pointer);
-    printf(1, "----%d----\n", *pointer);
-    printf(1, "<<<<PASS Task 1 Test >>>>\n");
+    test1();
   }
   if(*argv[1]=='0' || *argv[1]=='2'){
     test2();
